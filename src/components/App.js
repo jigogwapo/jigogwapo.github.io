a504b0b5d35a011217f5ec19adc3bfa5f8d62289
Jesreal Arcillas <jesrealarcillas@gmail.com>
2022-09-03 00:09:38 +0800

    added a link to my project

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to my website.</h1>
+    <div
+      className="ui center aligned container"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <div style={{ minWidth: '500px' }}>
+        <h1 className="ui top attached block header">JESREAL ARCILLAS</h1>
+        <div className="ui left aligned attached segment">
+          <h3 className="ui header">Projects:</h3>
+          <a
+            className="ui link"
+            href="https://jigogwapo.github.io/youtube-search-app/"
+          >
+            Youtube Search App
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
